faac5b54a40829fa49d00cb6cef706f2dbe4e460
Graham MacFarquhar <gmacfarquhar57@gmail.com>
2023-04-07 15:35:25 -0700

    avg-pair, construct-word

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,30 @@
-// add whatever parameters you deem necessary & write docstring
-function averagePair() {
+/**
+ * accept an array of numbers, nums, and a numeric targetAvg.
+ * Determine if there is a pair of numbers in nums where
+ * the average of the pair equals targetAvg. There may be more
+ * than one pair that matches the average target.
+ * @param {[number]} nums
+ * @param {number} targetAvg
+ * @returns {boolean}
+ */
+function averagePair(nums, targetAvg) {
+  if (nums.length === 0) return false;
+  
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    const avg = (nums[left] + nums[right]) / 2;
+
+    if (avg === targetAvg) {
+      return true;
+    } else if (avg > targetAvg) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+
+  return false;
 }
 
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,33 @@
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+/**
+ * accept two strings: word and letters.
+ * return true if the word can be built with the letters;
+ * otherwise, it should return false.
+ * @param {string} word
+ * @param {string} letters
+ * @return {boolean}
+ */
+function canConstructWord(word, letters) {
+  if(letters.length === 0 && word.length > 0) return false;
+
+  let freqWord = getFrequencyCounter(word);
+  let freqLetter = getFrequencyCounter(letters);
+
+  for(let key in freqWord){
+    if(freqLetter[key]<freqWord[key]){
+      return false;
+    }
+  }
+  return true;
+}
+
+function getFrequencyCounter(items) {
+  const freqs = {};
+
+  for (const item of items) {
+    const curr = freqs[item] || 0;
+    freqs[item] = curr + 1;
+  }
+
+  return freqs;
 }
